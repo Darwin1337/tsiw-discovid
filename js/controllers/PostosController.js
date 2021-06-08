c0f9d2324b5574a9e7bc4ee8812854f918f49de6
Gama10Tech <72747189+Gama10Tech@users.noreply.github.com>
2021-06-08 17:46:13 +0100

    Perfil Postos Update

--- js/controllers/PostosController.js
+++ js/controllers/PostosController.js
@@ -17,6 +17,55 @@ export default class PostosController {
     return this.localidades;
   }
 
+  EditPosto(id, nome, email, password, website, horario_inicio, horario_fim, intervalo_consulta, morada, cod_postal, drive_thru, call_me){
+    let x=true
+    // console.log(id)
+    // console.log(nome)
+    // console.log(nif)
+    // console.log(email)
+    // console.log(password)
+    // console.log(website)
+    // console.log(horario_inicio)
+    // console.log(horario_fim)
+    // console.log(intervalo_consulta)
+    // console.log(drive_thru)
+    // console.log(call_me)
+    for (let i = 0; i < this.postos.length; i++) {
+      if (this.postos[i]["id"]==id) {
+        x=false
+      }
+      if (x==false) {
+        this.postos[i].nome = nome;
+        //this.postos[i].nif = nif;
+        this.postos[i].email = email;
+        this.postos[i].password = password;
+        this.postos[i].website = website;
+        this.postos[i].horario_inicio = horario_inicio;
+        this.postos[i].horario_fim = horario_fim;
+        this.postos[i].intervalo_consulta = intervalo_consulta;
+        this.postos[i].drive_thru = drive_thru;
+        this.postos[i].call_me = call_me;
+        break;
+      }
+    }
+
+    if(x==false){
+      for (let i = 0; i < this.enderecos.length; i++) {
+        if (id==this.enderecos[i]["id_entidade"]) {
+          this.enderecos[i].morada=morada;
+          this.enderecos[i].cod_postal=cod_postal;
+        }
+      }
+      localStorage.removeItem("enderecos_entidade");
+      localStorage.setItem("enderecos_entidade", JSON.stringify(this.enderecos));
+      localStorage.removeItem("utilizadores_entidades");
+      localStorage.setItem("utilizadores_entidades", JSON.stringify(this.postos));
+      Swal.fire('Sucesso!', 'O posto foi alterado com sucesso!', 'success');
+      event.preventDefault()
+      setTimeout(function(){location.reload()},1500);
+    }
+}
+
   // AddTest(nome) {
   //   const newId = this.testes.length > 0 ? this.testes[this.testes.length - 1].id_teste + 1 : 1;
   //   this.testes.push(new Postos(newId, nome));
--- js/views/ProfileView.js
+++ js/views/ProfileView.js
@@ -1,23 +1,126 @@
 import UserController from '../controllers/UserController.js';
+import PostoController from '../controllers/PostosController.js';
 
 export default class NavbarView {
   constructor() {
     // Instanciar o UserController para ser possível aceder ao métodos dos utilizadores
     this.userController = new UserController();
 
+    this.postoController = new PostoController();
+
     // Event listeners para quando o utilizador pretende editar o seu perfil e guardar as suas alterações
     this.BindEditButtons();
 
     // Carregar a informação para os inputs;
-    this.SetProfileInfo();
+    
+
+    this.currentPage = document.querySelector("body");
+    if (this.currentPage.id == "editar-perfil") {
+      this.VerificarTipoEMostrar();
+      
+    }
+  }
+
+  VerificarTipoEMostrar(){
+    const userInfo = this.userController.getLoggedInUserType();
+    if (userInfo=="posto") {
+      document.querySelector("#campos-editar-perfil").innerHTML=`
+      <div class="col-md-6 mt-3">
+        <span>Nome</span>
+        <input type="text" class="input-login-registar" id="posto-nome" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>NIF</span>
+        <input type="text" class="input-login-registar"  id="posto-nif" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>E-mail</span>
+        <input type="text" class="input-login-registar" id="posto-email" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Palavra-Passe</span>
+        <input type="text" class="input-login-registar"  id="posto-password" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Website</span>
+        <input type="text" class="input-login-registar" id="posto-website" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Hora de abertura</span>
+        <input type="time" class="input-login-registar" id="posto-horario-abertura" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Hora de fecho</span>
+        <input type="time" class="input-login-registar" id="posto-horario-fecho" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Tempo de consulta</span>
+        <input type="text" class="input-login-registar" id="posto-tempo-consulta" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Morada</span>
+        <input type="text" class="input-login-registar" id="posto-morada" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Codigo Postal</span>
+        <input type="text" class="input-login-registar" id="posto-cod_postal" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Drive</span>
+        <input type="checkbox" class="input-login-registar" id="posto-drive" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Call Me</span>
+        <input type="checkbox" class="input-login-registar" id="posto-call" disabled value="">
+      </div>
+      `
+      this.SetProfileInfoPosto();
+      
+    }
+    else{
+      document.querySelector("#campos-editar-perfil").innerHTML=`
+      <div class="col-md-12 d-flex flex-column align-items-center align-self-center">
+        <img class="" id="avatar-profile">
+        <input type="text" class="mt-3" id="avatar-profile-text" disabled>
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Primeiro Nome</span>
+        <input type="text" class="input-login-registar" id="user-pnome" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>E-mail</span>
+        <input type="text" class="input-login-registar"  id="user-email" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Ultimo Nome</span>
+        <input type="text" class="input-login-registar" id="user-unome" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Palavra-Passe</span>
+        <input type="text" class="input-login-registar"  id="user-password" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Morada</span>
+        <input type="text" class="input-login-registar" id="user-morada" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Nº Telemovel</span>
+        <input type="text" class="input-login-registar" id="user-tlm" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>CEP</span>
+        <input type="text" class="input-login-registar" id="user-cep" disabled value="">
+      </div>
+      <div class="col-md-6 mt-3">
+        <span>Pontos Conquistados: <span id="user-pontos"></span></span>
+      </div>
+      `
+      this.SetProfileInfoNormal();
+    }
   }
 
   BindEditButtons() {
     document.getElementById("ativar-campos").addEventListener("click", () => {
-
-      // Bind ao botão de atualizar dados
-      this.UpdateUserInfo()
-
       for (const input of document.querySelectorAll("#campos-editar-perfil input")) {
         input.disabled = false;
       }
@@ -31,19 +134,76 @@ export default class NavbarView {
       for (const input of document.querySelectorAll("#campos-editar-perfil input")) {
         input.disabled = true;
       }
+      const userInfo = this.userController.getLoggedInUserType();
+      if (userInfo=="posto") {
+        this.UpdatePostosInfo()
+      }
+      else{
+        this.UpdateUserInfo()
+      }
     });
   }
 
-  SetProfileInfo() {
+  SetProfileInfoPosto() {
     const userInfo = this.userController.getLoggedInUserData();
-    document.getElementById("user-id").value = userInfo.id;
+    const userInfoEnderecos = this.userController.getAllEntityEnderecos();
+    for (let i = 0; i < userInfoEnderecos.length; i++) {
+      if (userInfo.id==userInfoEnderecos[i].id_entidade) {
+        document.getElementById("posto-morada").value = userInfoEnderecos[i].morada;
+        document.getElementById("posto-cod_postal").value = userInfoEnderecos[i].cod_postal;
+      }
+    }
+    document.getElementById("posto-nome").value = userInfo.nome;
+    document.getElementById("posto-email").value = userInfo.email;
+    document.getElementById("posto-nif").value = userInfo.nif;
+    document.getElementById("posto-password").value = userInfo.password;
+    document.getElementById("posto-website").value = userInfo.website;
+    document.getElementById("posto-horario-abertura").value = userInfo.horario_inicio;
+    document.getElementById("posto-horario-fecho").value = userInfo.horario_fim;
+    document.getElementById("posto-tempo-consulta").value = userInfo.intervalo_consulta;
+    document.getElementById("posto-drive").checked = userInfo.drive_thru;
+    document.getElementById("posto-call").checked = userInfo.call_me;
+  }
+
+  SetProfileInfoNormal() {
+    const userInfo = this.userController.getLoggedInUserData();
+    document.getElementById("avatar-profile").src = userInfo.avatar;
+    document.getElementById("avatar-profile-text").value = userInfo.avatar;
     document.getElementById("user-pnome").value = userInfo.pnome;
     document.getElementById("user-email").value = userInfo.email;
     document.getElementById("user-unome").value = userInfo.unome;
     document.getElementById("user-password").value = userInfo.password;
-    document.getElementById("user-tlm").value = userInfo.tlm;
-    document.getElementById("user-pontos").innerHTML = userInfo.pontos;
+    // document.getElementById("posto-website").value = userInfo.website;
+    // document.getElementById("posto-horario-abertura").value = userInfo.horario_inicio;
+    // document.getElementById("posto-horario-fecho").value = userInfo.horario_fim;
+    // document.getElementById("posto-tempo-consulta").value = userInfo.intervalo_consulta;
+    // document.getElementById("posto-drive").checked = userInfo.drive_thru;
+    // document.getElementById("posto-call").checked = userInfo.call_me;
+    
     //document.getElementById("user-cep").value=this.userController.getLoggedInUserData().email;
+  }
+
+  UpdatePostosInfo() {
+    try {
+      const userInfo = this.userController.getLoggedInUserData();
+      this.postoController.EditPosto(
+        userInfo.id,
+        document.getElementById("posto-nome").value,
+        //document.getElementById("posto-nif").value,
+        document.getElementById("posto-email").value,
+        document.getElementById("posto-password").value,
+        document.getElementById("posto-website").value,
+        document.getElementById("posto-horario-abertura").value,
+        document.getElementById("posto-horario-fecho").value,
+        document.getElementById("posto-tempo-consulta").value,
+        document.getElementById("posto-morada").value,
+        document.getElementById("posto-cod_postal").value,
+        document.getElementById("posto-drive").checked,
+        document.getElementById("posto-call").checked
+        )
+    } catch (e) {
+      Swal.fire('Erro!', String(e).substring(7), 'error');
+    }
   }
 
   //falta aqui!!!!!!
